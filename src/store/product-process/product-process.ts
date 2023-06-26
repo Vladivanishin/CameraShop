12af16d5c722a7a1e4569d86894436fc21d37c05
Vladislav Ivanishin <skytex51rus@gmail.com>
2023-06-27 00:14:52 +1000

    dont finish: no test, validation etd

--- src/store/product-process/product-process.ts
+++ src/store/product-process/product-process.ts
@@ -1,27 +1,42 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { NameSpace } from '../../conts';
-import { Cameras } from '../../types/catalog';
-import { fetchSimilarAction } from '../api-actions';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { NameSpace, TabsControl } from '../../conts';
+import { Cameras, ReviewResponse } from '../../types/catalog';
+import { fetchPostReviewAction, fetchReviewsAction, fetchSimilarAction } from '../api-actions';
 
 type ProductProcess = {
   similarCameras: Cameras;
-
+  currentTabControl: TabsControl;
+  reviews: ReviewResponse[];
+  reviewSuccess: boolean;
 }
 
 const initialState: ProductProcess = {
   similarCameras: [],
+  currentTabControl: TabsControl.Description,
+  reviews: [],
+  reviewSuccess: false,
 };
 
 export const productProcess = createSlice({
   name: NameSpace.Catalog,
   initialState,
   reducers: {
+    selectTabsControl: (state, action: PayloadAction<TabsControl>) => {
+      state.currentTabControl = action.payload;
+    }
   },
   extraReducers(builder) {
     builder
       .addCase(fetchSimilarAction.fulfilled, (state, action) => {
         state.similarCameras = action.payload;
+      })
+      .addCase(fetchReviewsAction.fulfilled, (state, action) => {
+        state.reviews = action.payload;
+      })
+      .addCase(fetchPostReviewAction.fulfilled, (state, action) => {
+        state.reviews.push(action.payload);
       });
   },
 });
 
+export const { selectTabsControl } = productProcess.actions;
